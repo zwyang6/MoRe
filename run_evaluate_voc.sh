82e4dc11873853c09c56bcf855dd53f9df287cd6
Debug_Yann <zwyang21@m.fudan.edu.cn>
2024-12-24 11:42:29 +0800

    Add files via upload

--- run_evaluate_voc.sh
+++ run_evaluate_voc.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
-echo "Evaluating VOC seg Results>>>>>>>>>>>"
-refine_with_multiscale=true
-crf_post=true
-cam2seg=true
+device=$1
+nproc_per_node=$2
+checkpoint=$1
 
-file=$1
-device=$2
-nproc_per_node=$3
-infer_set=$4
-checkpoint=$5
-
-
-CUDA_VISIBLE_DEVICES=$device python -m torch.distributed.launch --nproc_per_node=$nproc_per_node  $file --model_path $checkpoint --infer_set $infer_set --refine_with_multiscale $refine_with_multiscale  --crf_post $crf_post --cam2seg $cam2seg
-
-echo "Evaluating VOC LAM Results>>>>>>>>>>>"
+echo "Evaluating VOC CAM Results>>>>>>>>>>>"
+file=./tools/infer_cam.py
+infer_set=train
 cam2seg=false
 CUDA_VISIBLE_DEVICES=$device python -m torch.distributed.launch --nproc_per_node=$nproc_per_node  $file --model_path $checkpoint --infer_set $infer_set --refine_with_multiscale $refine_with_multiscale  --crf_post $crf_post --cam2seg $cam2seg
 
+
+echo "Evaluating VOC LAM Results>>>>>>>>>>>"
+file=./tools/infer_lam.py
+CUDA_VISIBLE_DEVICES=$device python -m torch.distributed.launch --nproc_per_node=$nproc_per_node  $file --model_path $checkpoint --infer_set $infer_set --refine_with_multiscale $refine_with_multiscale  --crf_post $crf_post --cam2seg $cam2seg
+
+
+echo "Evaluating VOC seg Results>>>>>>>>>>>"
+file=./tools/infer_seg_voc.py
+infer_set=val
+CUDA_VISIBLE_DEVICES=$device python $file --model_path $checkpoint --infer_set $infer_set
