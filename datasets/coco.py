e917331c3e7ed71c4365bd3caa0599b7c93e481f
Debug_Yann <zwyang21@m.fudan.edu.cn>
2025-01-15 11:46:52 +0800

    Add files via upload

--- datasets/coco.py
+++ datasets/coco.py
@@ -1,11 +1,16 @@
 import numpy as np
 import torch.nn.functional as F
 from torch.utils.data import Dataset
+import torch
 import os
 import imageio
+import sys
 from . import transforms
 from PIL import Image
 from torchvision import transforms as T
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+
 
 class_list = ['_background_', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus', 'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'stop sign', 'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow', 'elephant', 'bear', 'zebra', 'giraffe', 'backpack', 'umbrella', 'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball', 'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket', 'bottle', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl', 'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza', 'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'dining table', 'toilet', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone', 'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'book', 'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush']
 
@@ -26,17 +31,17 @@ def robust_read_image(image_name):
 class CocoDataset(Dataset):
     def __init__(
         self,
-        img_dir=None,
-        label_dir=None,
+        root_dir=None,
         name_list_dir=None,
         split='train',
         stage='train',
     ):
         super().__init__()
 
-        self.img_dir = img_dir
-        self.label_dir = label_dir
+        self.root_dir = root_dir
         self.stage = stage
+        self.img_dir = os.path.join(root_dir, 'JPEGImages')
+        self.label_dir = os.path.join(root_dir, 'SegmentationClass')
         self.name_list_dir = os.path.join(name_list_dir, split + '.txt')
         self.name_list = load_img_name_list(self.name_list_dir)
 
@@ -49,9 +54,16 @@ class CocoDataset(Dataset):
 
         if self.stage == "train":
             img_name = os.path.join(self.img_dir, "train", _img_name+'.jpg')
-            image = np.asarray(robust_read_image(img_name))
-            label_dir = os.path.join(self.label_dir, "train", _img_name[15:]+'.png')
-            label = np.asarray(imageio.imread(label_dir))
+            if not os.path.exists(img_name):
+                img_name = os.path.join(self.img_dir, "val", _img_name+'.jpg')
+                image = np.asarray(robust_read_image(img_name))
+                label_dir = os.path.join(self.label_dir, "val", _img_name[13:]+'.png')
+                # label = np.asarray(imageio.imread(label_dir))
+                label = np.asarray(Image.open(label_dir))
+            else:
+                image = np.asarray(robust_read_image(img_name))
+                label_dir = os.path.join(self.label_dir, "train", _img_name[15:]+'.png')
+                label = np.asarray(Image.open(label_dir))
 
         elif self.stage == "val":
             img_name = os.path.join(self.img_dir, "val", _img_name+'.jpg')
@@ -70,147 +82,69 @@ class CocoDataset(Dataset):
 class CocoClsDataset(CocoDataset):
     def __init__(
         self,
-        img_dir=None,
-        label_dir=None,
-        name_list_dir=None,
-        split='train',
-        stage='train',
-        resize_range=[512, 640],
-        rescale_range=[0.5, 2.0],
-        crop_size=512,
-        img_fliplr=True,
+        root_dir,
+        name_list_dir,
+        split="train",
+        stage="train",
+        crop_size=448,
+        num_classes=81,
         ignore_index=255,
-        num_classes=21,
-        aug=False,
-        **kwargs):
-
-        super().__init__(img_dir, label_dir, name_list_dir, split, stage)
-
-        self.aug = aug
-        self.ignore_index = ignore_index
-        self.resize_range = resize_range
-        self.rescale_range = rescale_range
+        transform=None,
+        aug=True
+    ):
+        super().__init__(root_dir, name_list_dir, split, stage)
         self.crop_size = crop_size
-        self.local_crop_size = 96
-        self.img_fliplr = img_fliplr
         self.num_classes = num_classes
-        # self.color_jittor = transforms.PhotoMetricDistortion()
+        self.ignore_index = ignore_index
 
-        self.gaussian_blur = transforms.GaussianBlur
-        self.solarization = transforms.Solarization(p=0.2)
-
+        self.transform = transform
+        self.random_hflip = T.RandomHorizontalFlip(p=0.5)
+        self.random_color_jitter = T.RandomApply([T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)], p=0.8)
+        self.aug = aug
         self.label_list = load_cls_label_list(name_list_dir=name_list_dir)
 
-        self.normalize = T.Compose([
-            T.ToTensor(),
-            T.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        ])
-
-        self.flip_and_color_jitter = T.Compose([
-            T.RandomHorizontalFlip(p=0.5),
-            T.RandomApply(
-                [T.ColorJitter(brightness=0.4, contrast=0.4, saturation=0.2, hue=0.1)],
-                p=0.8
-            ),
-            T.RandomGrayscale(p=0.2),
-        ])
-        
-        self.global_view1 = T.Compose([
-        ])
-        self.global_view2 = T.Compose([
-            T.RandomResizedCrop(self.crop_size, scale=[0.4, 1], interpolation=Image.BICUBIC),
-            self.flip_and_color_jitter,
-            self.gaussian_blur(p=0.1),
-            self.solarization,
-            self.normalize,
-        ])
-        self.local_view = T.Compose([
-            # T.RandomResizedCrop(self.local_crop_size, scale=[0.4, 1], interpolation=Image.BICUBIC),
-            self.flip_and_color_jitter,
-            self.gaussian_blur(p=0.5),
-            self.normalize,
-        ])
 
     def __len__(self):
         return len(self.name_list)
 
-    def __transforms(self, image):
-        img_box = None
-        local_image = None
-        if self.aug:
-
-            if self.rescale_range:
-                image = transforms.random_scaling(image, scale_range=self.rescale_range)
-            if self.img_fliplr:
-                image = transforms.random_fliplr(image)
-            if self.crop_size:
-                image, img_box = transforms.random_crop(image, crop_size=self.crop_size, mean_rgb=[0,0,0], ignore_index=self.ignore_index)
-            
-            local_image = self.local_view(Image.fromarray(image))
-            image = self.global_view1(Image.fromarray(image))
-        
-        image = self.normalize(image)
-        
-        return image, local_image, img_box
-
-    @staticmethod
-    def _to_onehot(label_mask, num_classes, ignore_index):
-        #label_onehot = F.one_hot(label, num_classes)
-        
-        _label = np.unique(label_mask).astype(np.int16)
-        # exclude ignore index
-        _label = _label[_label != ignore_index]
-        # exclude background
-        _label = _label[_label != 0]
-
-        label_onehot = np.zeros(shape=(num_classes), dtype=np.uint8)
-        label_onehot[_label] = 1
-        return label_onehot
+    def transform_image(self, img):
+        img = self.random_color_jitter(img)
+        img = np.array(img)
+        img = transforms.random_scaling(img, scale_range=[0.32, 1.0])
+        img = transforms.random_fliplr(img)
+        img, img_box = transforms.random_crop(img,crop_size=self.crop_size, mean_rgb=[0, 0, 0], ignore_index=self.ignore_index)
+        img = transforms.normalize_img(img)
+        # plt.imshow(img)
+        # plt.show()
+        img = np.transpose(img, (2, 0, 1))  # HWC -> CHW
+        return img, img_box
 
     def __getitem__(self, idx):
 
-        img_name, image, _ = super().__getitem__(idx)
-        
+        name, image, label = super().__getitem__(idx)
+        cls_label = self.label_list[name]
         pil_image = Image.fromarray(image)
 
-        image, local_image, img_box = self.__transforms(image=image)
+        img, img_box = self.transform_image(pil_image)
 
-        cls_label = self.label_list[img_name]
-
-        # co-occurence is -1 while one cls is saved
-        label_idx = np.zeros(1,)
-        if cls_label.sum(-1) != 0:
-            label_idx = np.where(cls_label.sum(-1)>1,-1,np.argmax(cls_label,axis=-1)+1)
-        else:
-            label_idx = np.array([-1])
-
-        if self.aug:
-
-            raw_image = image
-            w_image = self.global_view2(pil_image)
-            s_image = local_image
-
-            return img_name, image, cls_label,label_idx, img_box, raw_image, w_image, s_image
-        else:
-            return img_name, image, cls_label,label_idx
-
+        return name, img, cls_label, img_box
 
 class CocoSegDataset(CocoDataset):
     def __init__(self,
-                 img_dir=None,
-                 label_dir=None,
+                 root_dir=None,
                  name_list_dir=None,
                  split='train',
                  stage='train',
                  resize_range=[512, 640],
                  rescale_range=[0.5, 2.0],
-                 crop_size=512,
+                 crop_size=448,
                  img_fliplr=True,
                  ignore_index=255,
                  aug=False,
                  **kwargs):
 
-        super().__init__(img_dir, label_dir, name_list_dir, split, stage)
+        super().__init__(root_dir, name_list_dir, split, stage)
+
 
         self.aug = aug
         self.ignore_index = ignore_index
